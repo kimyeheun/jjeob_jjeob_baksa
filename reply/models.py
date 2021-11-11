df08cb50ab056c495f3a19376a3433b8150b78d8
Kim-02 <80444565+Kim-02@users.noreply.github.com>
2021-11-11 21:21:29 +0900

    Update models.py

--- reply/models.py
+++ reply/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class reply(models.Model):
-    user_id = models.ForeignKey(max_length=10)
+    user_id = models.ForeignKey(on_delete=models.SET_NULL)
     post_date = models.DateField()
     modify_date = models.DateField()
     reply_desc = models.TextField()
