b7f4e6cb2a3a300b50b2b298415c32358c8ee530
Kim-02 <80444565+Kim-02@users.noreply.github.com>
2021-11-11 21:12:59 +0900

    Update models.py

--- reply/models.py
+++ reply/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class reply(models.Model):
+    user_id = models.CharField(max_length=10)
+    post_date = models.DateField()
+    modify_date = models.DateField()
+    reply_desc = models.TextField()
+    
 # Create your models here.
