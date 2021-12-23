aa2ec29d1a9ec08beaf7e393a88f9378ff12ad5d
김예은 <yeheun004@gmail.com>
2021-12-23 22:58:22 +0900

    원상복구

--- user/models.py
+++ user/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+class MyAccountManager(BaseUserManager):
+    def create_user(self, user_email, user_name, user_id, password=None):
+        if not user_email:
+            raise ValueError("이메일은 필수 입력 사항 입니다.")
+        if not user_name:
+            raise ValueError("이름은 필수 입력 사항 입니다.")
+        if not user_id:
+            raise ValueError("아이디는 필수 입력 사항 입니다.")
+
+        user = self.model(
+            user_email=self.normalize_email(user_email),
+            user_name=user_name,
+            user_id=user_id,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, user_email, user_name, user_id, password=None):
+        user = self.create_user(
+            user_email=self.normalize_email(user_email),
+            password=password,
+            user_name=user_name,
+            user_id=user_id,
+        )
+        user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
+
+class Account(AbstractBaseUser):
+    user_id = models.IntegerField(null=False, primary_key=True)
+    user_name = models.CharField(max_length=50, null=False, verbose_name="유저 이름")
+    password = models.CharField(max_length=1000, null=False)
+    user_nick = models.CharField(max_length=50, null=True, blank=True, verbose_name="유저 닉네임")
+    user_category = models.BooleanField(default=True, verbose_name="유저 카테고리")
+    user_email = models.CharField(verbose_name="이메일", max_length=50, null=False, unique=True,)
+    food_reco = models.IntegerField(null=True, blank=True, verbose_name="음식 추천수")
+    user_pic = models.ImageField(null=True, blank=True, verbose_name="유저 프로필 사진")
+
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'user_email'
+    REQUIRED_FIELDS = ['user_id', 'user_name', ]
+
+    objects = MyAccountManager()
+
+    def __str__(self):
+        return self.user_email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
--- user/views.py
+++ user/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from .models import Account
 # Create your views here.
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            print("인증성공")
+            login(request, user)
+        else:
+            print("인증실패")
+        
+    return render(request, "USER/login.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect("USER:login")
+
+def signup_view(request):
+    if request.method == "POST":
+        user_id = request.POST["user_id"]
+        user_name = request.POST["user_name"]
+        password = request.POST["password"]
+        user_email = request.POST["user_email"]
+        user_nick = request.POST["user_nick"]
+
+        user = Account.objects.create(user_name, user_email, password)
+        user.user_id = user_id
+        user.user_nick = user_nick
+        user.save()
+        return redirect("USER:login")
+
+    return render(request, "USER/signup.html")
